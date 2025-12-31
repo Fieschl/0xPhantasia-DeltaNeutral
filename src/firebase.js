e06dc830f57ff5bad7ea32b6f7228ef155587a2e
hkennandya-dev <nayanika.model@gmail.com>
2025-12-31 10:28:53 +0700

    fix:depedency, firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, signInAnonymously as signInAnonymouslyOriginal, onAuthStateChanged as onAuthStateChangedOriginal } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -15,3 +15,5 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const signInAnonymously = signInAnonymouslyOriginal;
+export const onAuthStateChanged = onAuthStateChangedOriginal;
